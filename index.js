fdf5e3d3b4e5f1e3fa19521fdac1ebdd47feafc3
marinm <mestrovic.ma@gmail.com>
2017-10-03 10:16:23 -0400

    refactor/index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const path = require('path');
 const http = require('http');
-const bodyParser = require('body-parser');
 const Express = require('express');
 const SocketIO = require('socket.io');
 
@@ -8,13 +6,6 @@ const SocketIO = require('socket.io');
 const app = new Express();
 const server = http.Server(app);
 let io = new SocketIO(server);
-
-const publicPath = path.join(__dirname, '/client');
-
-// define the folder that will be used for static assets
-app.use(Express.static(path.join(publicPath, 'static')));
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 io.on('connection', client => {
   client.on('client message', msg => {
@@ -38,8 +29,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// universal routing and rendering
-app.get('*', (req, res) => {});
 
 // start the server
 const port = process.env.PORT || 5000;
