a580d90ad4d2b2a4ce6c9e17aaee053c6ba76c6f
Larry Faudree <lfaudreejr@hotmail.com>
2017-10-07 13:17:48 -0400

    feature / added apiai request and response to websocket.js

--- server/websocket.js
+++ server/websocket.js
@@ -1,15 +1,23 @@
-const server = require('./')
-const SocketIO = require('socket.io');
+const server = require("./");
+const SocketIO = require("socket.io");
+const apiai = require("apiai");
+const apiController = apiai(process.env.APIAI_KEY);
 
 function createSocket(server) {
   let io = new SocketIO(server);
-
-  io.on('connection', client => {
-    client.on('client message', msg => {
-      console.log(msg);
-      client.emit('bot message', 'hello from server');
+  io.on("connection", client => {
+    client.on("client message", msg => {
+      let request = apiController.textRequest(msg, {
+        sessionId: "timebot"
+      });
+      request.on("response", response => {
+        client.emit("bot message", response);
+      });
+      request.on("error", error => {
+        client.emit("bot message", error);
+      });
+      request.end();
     });
   });
 }
-
 module.exports = createSocket;
