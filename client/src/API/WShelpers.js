6fc753b4113d24c43f1004e403425e6f72bbcee4
mmarinm <mestrovic.ma@gmail.com>
2017-10-28 17:57:09 -0400

    update/ development

--- client/src/API/WShelpers.js
+++ client/src/API/WShelpers.js
@@ -3,10 +3,9 @@ const socket = openSocket('http://localhost:5000');
 
 //messages travel in real time thats why we don't need async await
 // but will be necessary when we add async api request in index.js
-function getBotMessage(fn) {
+function getBotMessage(cb) {
   socket.on('bot message', msg => {
-    console.log(msg);
-    fn(msg);
+    cb(msg);
   });
 }
 
